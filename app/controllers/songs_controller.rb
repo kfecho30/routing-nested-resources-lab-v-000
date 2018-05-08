5b01ae270374b7f82fad6f592692b31efae360c4
Kelsey Fecho <kfecho30@gmail.com>
2018-05-08 17:53:46 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      @artist = Artist.find_by(id: params[:id])
+      @artist = Artist.find_by(id: params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
     else
       flash[:alert] = "Song not found"
